ff0683693cec9477d7a7269061edf0e7896a1c61
agrawalsajal02 <34278780+agrawalsajal02@users.noreply.github.com>
2018-12-10 11:40:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,204 @@
+
+    
+    var total=0;
+        var sel=0;
+        
+        function fun1(){
+            sel++;
+            var k=total+950000;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+         function fun2(){
+            sel++;
+            var k=total+450000;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+      function fun3(){
+            sel++;
+            var k=total+120000;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+    
+    
+    
+      function fun4(){
+            sel++;
+            var k=total+5000;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+    
+    
+    
+      function fun5(){
+            sel++;
+            var k=total+4000;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+    
+    
+    
+      function fun6(){
+            sel++;
+            var k=total+8000;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+    
+    
+    
+      function fun7(){
+            sel++;
+            var k=total+95000;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+    
+    
+    
+      function fun8(){
+            sel++;
+            var k=total+95000;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+    
+    
+      function fun9(){
+            sel++;
+            var k=total+12000;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+    
+    
+    
+      function fun10(){
+            sel++;
+            var k=total+9500;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+    
+    
+    
+      function fun11(){
+            sel++;
+            var k=total+4500;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+    
+    
+    
+      function fun12(){
+            sel++;
+            var k=total+10000;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+    
+    
+    
+      function fun13(){
+            sel++;
+            var k=total+5000;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+    
+    
+    
+      function fun14(){
+            sel++;
+            var k=total+4520;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+    
+    
+    
+      function fun15(){
+            sel++;
+            var k=total+5620;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+    
+    
+    
+      function fun16(){
+            sel++;
+            var k=total+50000;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+    
+    
+    
+      function fun17(){
+            sel++;
+            var k=total+20000;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+    
+    
+    
+      function fun18(){
+            sel++;
+            var k=total+12000;
+            document.getElementById("total").innerHTML="Total price:"+k;
+            total=k;
+            document.getElementById("price").innerHTML="Selected :"+sel;
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
